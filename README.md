e1c266293320fe2c3f094614afd57c3499ce695a
Jorge Ferrando <jorfermo@gmail.com>
2022-02-03 20:22:47 +0100

    feat: even producer

---
+++ README.md
@@ -0,0 +1,14 @@
+# kafka POC
+
+Launch kafka + zookeeper locally
+
+```bash
+docker compose up
+```
+
+Reading events from kafka topic `test`
+
+```bash
+brew install kafka
+kafka-console-consumer --bootstrap-server localhost:29092 --topic test --from-beginning
+```
--- src/main/java/com/jorfermo/kafkapoc/KafkaPocApplication.java
+++ src/main/java/com/jorfermo/kafkapoc/KafkaPocApplication.java
@@ -1,13 +1,23 @@
 package com.jorfermo.kafkapoc;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.kafka.core.KafkaTemplate;
 
 @SpringBootApplication
 public class KafkaPocApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(KafkaPocApplication.class, args);
+	}
+
+	@Bean
+	CommandLineRunner commandLineRunner(KafkaTemplate<String, String> kafkaTemplate) {
+		return args -> {
+			kafkaTemplate.send("test", "Hello World");
+		};
 	}
 
 }
