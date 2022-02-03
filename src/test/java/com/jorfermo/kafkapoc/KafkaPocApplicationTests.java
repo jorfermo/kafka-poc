1b92f50dcf8b1fffe24fc27827ea03de7d1fa6f8
Jorge Ferrando <jorfermo@gmail.com>
2022-02-03 19:51:01 +0100

    feat: bootstrap service

---
+++ src/test/java/com/jorfermo/kafkapoc/KafkaPocApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jorfermo.kafkapoc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaPocApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
